7e89baab9fb84efe9080890ad6935f551244b6f0
Vishal Singh <VishalSingh1597>
2021-05-11 23:49:36 +0530

    [Vishal]Added;Creating Stack

---
+++ UnitTestStacksAndQueue/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestStacksAndQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestStacksAndQueue")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5cd6f07-9ce9-4f39-a54e-bb8a64d84e42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestStacksAndQueue/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StacksAndQueue;
+using System.Collections;
+
+
+namespace UnitTestStacksAndQueue
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Stack stack = new Stack(); //create object of Stack class
+
+        [TestMethod]
+        public void PushStackElement()
+        {
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+        }
+
+    }
+}
