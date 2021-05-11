47d4a974598119486865ea676d248240bc55f192
Vishal Singh <VishalSingh1597>
2021-05-12 00:09:41 +0530

    [Vishal]Added:Creating Stack

--- StacksAndQueue/Program.cs
+++ StacksAndQueue/Program.cs
@@ -10,6 +10,13 @@ namespace StacksAndQueue
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Stack");//Print 
+            Stack stack = new Stack(); //create object of Stack class
+            stack.Push(56); //call Push method Push element in stack 
+            stack.Push(30);//call Push method Push element in stack  
+            stack.Push(70);// call Push method Push element in stack 
+            stack.Display();//call Display method Display element in stack 
+            Console.ReadLine();
         }
     }
 }
